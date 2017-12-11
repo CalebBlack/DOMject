3585e2c85009c533e3fc7975782845199248ec8c
Caleb Black <caleb.paul.black@gmail.com>
2017-12-10 20:23:27 -0700

    call super in domject

--- src/domject.js
+++ src/domject.js
@@ -1,12 +1,13 @@
 class DOMject extends Array {
   constructor(parent,HTML){
+    super(HTML);
     if (!(parent instanceof DOMject || parent instanceof HTMLElement));
     this.parent = parent;
     if (HTML) {
       this.element = HTML.element;
-      HTML.forEach((element,index)=>{
-        this[index] = element;
-      });
+      // HTML.forEach((element,index)=>{
+      //   this[index] = element;
+      // });
     } else {
       this.element = document.createElement('div');
     }
