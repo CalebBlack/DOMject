b492c5283f3482cf63795b0e4f1f7e27caefc74c
Caleb Black <caleb.paul.black@gmail.com>
2017-12-10 21:19:18 -0700

    remove bundle and update index.js

--- example/index.js
+++ example/index.js
@@ -3,12 +3,20 @@ const DOMject = require('../src/index.js');
 window.addEventListener('load',()=>{
   let code = document.createElement('input');
   let execute = document.createElement('button');
+  let logs = document.createElement('div');
   execute.textContent = 'Run';
   execute.addEventListener('click',()=>{
     let command = code.value;
     code.value = '';
-    if (typeof command == 'string' && command.length > 0) console.log(eval(command));
+    if (typeof command == 'string' && command.length > 0) {
+      let output = eval(command);
+      console.log(output);
+      let log = document.createElement('span');
+      log.textContent = output;
+      logs.appendChild(log);
+    }
   });
   document.body.appendChild(code);
   document.body.appendChild(execute);
+  document.body.appendChild(logs);
 });
