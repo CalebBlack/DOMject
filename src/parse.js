530b0b1288d488ec019cbac4e8b4408503f1fce1
Caleb Black <caleb.paul.black@gmail.com>
2017-12-10 17:31:06 -0700

    update parse

--- src/parse.js
+++ src/parse.js
@@ -1,5 +1,12 @@
 const DOMject = require('./domject');
-function parse(HTML,parent){
+const validator = require('html-validator');
 
+function parse(HTML,parent){
+  if (typeof HTML != 'string') throw new Error('DOMject: Could Not Parse, HTML not string.');
+  validator({format: 'html',data=HTML}).then(data=>{
+
+  }).catch(err=>{
+    throw new Error('DOMject: Could Not Parse, Invalid HTML.');
+  });
 }
 module.exports = HTML;
