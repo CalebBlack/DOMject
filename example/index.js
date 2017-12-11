954a5523a57775bd91eb03d8a2a533e5b34a1512
Caleb Black <caleb.paul.black@gmail.com>
2017-12-10 21:29:09 -0700

    update example

--- example/index.js
+++ example/index.js
@@ -2,10 +2,13 @@ const DOMject = require('../src/index.js');
 
 window.addEventListener('load',()=>{
   let code = document.createElement('input');
+  code.className='code';
   let execute = document.createElement('button');
+  execute.className='execute';
   let logs = document.createElement('div');
+  logs.className='logs';
   execute.textContent = 'Run';
-  execute.addEventListener('click',()=>{
+  let submit = ()=>{
     let command = code.value;
     code.value = '';
     if (typeof command == 'string' && command.length > 0) {
@@ -13,9 +16,12 @@ window.addEventListener('load',()=>{
       console.log(output);
       let log = document.createElement('span');
       log.textContent = output;
+      log.className='log';
       logs.appendChild(log);
     }
-  });
+  };
+  execute.addEventListener('click',submit);
+  code.addEventListener('keypress',e=>{if(e.keyCode === 13) submit()});
   document.body.appendChild(code);
   document.body.appendChild(execute);
   document.body.appendChild(logs);
