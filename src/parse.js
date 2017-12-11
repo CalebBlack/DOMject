6672dde034bf87c12726f07b0bbecc8e03182ecf
Caleb Black <caleb.paul.black@gmail.com>
2017-12-10 17:32:56 -0700

    filter empty strings from parse

--- src/parse.js
+++ src/parse.js
@@ -3,8 +3,9 @@ const validator = require('html-validator');
 
 function parse(HTML,parent){
   if (typeof HTML != 'string') throw new Error('DOMject: Could Not Parse, HTML not string.');
+  if (HTML.length < 1) throw new Error('DOMject: Could Not Parse, HTML string empty.');
   validator({format: 'html',data=HTML}).then(data=>{
-
+    
   }).catch(err=>{
     throw new Error('DOMject: Could Not Parse, Invalid HTML.');
   });
