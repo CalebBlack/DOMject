aef6b061e9a9c378bbe558a67bb68bb32920ac81
Caleb Black <caleb.paul.black@gmail.com>
2017-12-10 18:06:02 -0700

    fix incorrect export on parse

--- src/html/parse.js
+++ src/html/parse.js
@@ -5,6 +5,6 @@ function parse(HTML,parent){
   if (typeof HTML != 'string') throw new Error('DOMject: Could Not Parse, HTML not string.');
   if (HTML.length < 1) throw new Error('DOMject: Could Not Parse, HTML string empty.');
   if (!validate(HTML)) throw new Error('DOMject: Could Not Parse, HTML invalid.');
-  
+
 }
-module.exports = HTML;
+module.exports = parse;
